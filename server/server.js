362530f2881b11b792581cea801c9398573e1522
Aakarsh Sinha <aakarshsinha.in@gmail.com>
2024-06-23 10:45:36 +0530

    Setting directory structure

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import express from "express";
+import dotenv from "dotenv";
+
+connectDB();
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (err) => {
+  if (err) console.log("Error in server setup");
+  console.log(
+    `Server listening in ${process.env.NODE_ENV} mode on Port ${PORT}`
+  );
+});
