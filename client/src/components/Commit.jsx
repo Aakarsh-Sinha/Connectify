e95c3ef78e93cceeae9dbc5eea7b24ffcf89d02a
Chidam <chidambaram.suresh2022@vitstudent.ac.in>
2024-06-28 18:53:54 +0700

    What I did

--- client/src/components/Commit.jsx
+++ client/src/components/Commit.jsx
@@ -1,1 +1,14 @@
 // Changed object
+import React from 'react';
+
+const MyComponent = ({ name, age }) => {
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>You are {age} years old.</p>
+      <p>This is a JSX component example.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
