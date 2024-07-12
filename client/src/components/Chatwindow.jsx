1871cdecc799e0fa1c98bc87f0d65a88ce99c851
ashank1414 <ashank.sethi2022@vitstudent.ac.in>
2024-07-12 15:13:57 +0530

    fixed msg

--- client/src/components/Chatwindow.jsx
+++ client/src/components/Chatwindow.jsx
@@ -100,11 +100,11 @@ function Chatwindow({ active }) {
     <div className="flex-1 flex flex-col">
       <div className="flex p-[2vw] text-4xl font-bold border-b-2 border-gray-800">
         <div>
-        <img src={active.pfp} alt="Profile" className="rounded-full w-12 h-12" />
+          <img src={active.pfp} alt="Profile" className="rounded-full w-12 h-12" />
         </div>
         <div className="ml-[2vw]">{active.username}</div>
       </div>
-      <div id="messages-container" className="flex gap-5 h-[90%] flex-col-reverse px-[2vw] overflow-y-auto">
+      <div id="messages-container" className="flex flex-col gap-5 h-[90%] px-[2vw] overflow-y-auto">
         {messages.length === 0 ? (
           <div>No messages found</div>
         ) : (
@@ -123,9 +123,9 @@ function Chatwindow({ active }) {
           onChange={e => setSendmsg(e.target.value)}
         />
         <button className='ml-6' onClick={sendmessage}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
-        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
-        </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+          </svg>
         </button>
       </div>
     </div>
